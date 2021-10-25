66b16e0e3121a319da0b6457c9d6ed6fc9859b06
ItsMeLuka <Grubkiller00289@gmail.com>
2021-10-25 18:41:32 +0200

    Poczatki

---
+++ game.py
@@ -0,0 +1,33 @@
+import pygame
+import math
+from pygame import Surface, mixer
+
+# Initialize the game
+pygame.init()
+
+# Screen
+screen = pygame.display.set_mode((1820, 980))
+
+# Background
+background = pygame.image.load('background.png')
+
+# Brick
+brick = pygame.image.load('brick.png') 
+x = 0
+# Game Loop
+running = True
+while running:
+
+    screen.blit(background, (0, 0))
+
+    num_of_bricks = 91
+    for i in range(num_of_bricks):
+        x += 20
+        screen.blit(brick, (x, 780))
+        pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
